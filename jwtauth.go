79a089e8363309bd40f8a01449150134f55c46cc
Peter Kieltyka <peter.kieltyka@pressly.com>
2014-10-03 22:25:03 -0400

    Add Handle() function that accepts jwt param aliases, useful for oauth state stuff

--- jwtauth.go
+++ jwtauth.go
@@ -28,51 +28,68 @@ func New(alg string, signKey []byte, verifyKey []byte) *jwtAuth {
 	}
 }
 
-func (ja *jwtAuth) Handler(c *web.C, h http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		if c.Env == nil {
-			c.Env = make(map[string]interface{})
-		}
-
-		var tokenStr string
-		var err error
-
-		// Get token from query params
-		tokenStr = r.URL.Query().Get("jwt")
-
-		// Get token from authorization header
-		if tokenStr == "" {
-			bearer := r.Header.Get("Authorization")
-			if len(bearer) > 7 && strings.ToUpper(bearer[0:6]) == "BEARER" {
-				tokenStr = bearer[7:]
+func (ja *jwtAuth) Handle(paramAliases ...string) func(http.Handler) http.Handler {
+	f := func(h http.Handler) http.Handler {
+		fn := func(c web.C, w http.ResponseWriter, r *http.Request) {
+			if c.Env == nil {
+				c.Env = make(map[string]interface{})
 			}
-		}
 
-		// Get token from cookie
-		if tokenStr == "" {
-			cookie, err := r.Cookie("jwt")
-			if err == nil {
-				tokenStr = cookie.Value
+			var tokenStr string
+			var err error
+
+			// Get token from query params
+			tokenStr = r.URL.Query().Get("jwt")
+
+			// Get token from other query param aliases
+			if tokenStr == "" && paramAliases != nil && len(paramAliases) > 0 {
+				for _, p := range paramAliases {
+					tokenStr = r.URL.Query().Get(p)
+					if tokenStr != "" {
+						break
+					}
+				}
 			}
-		}
 
-		// Token is required, cya
-		if tokenStr == "" {
-			err = errUnauthorized
-		}
+			// Get token from authorization header
+			if tokenStr == "" {
+				bearer := r.Header.Get("Authorization")
+				if len(bearer) > 7 && strings.ToUpper(bearer[0:6]) == "BEARER" {
+					tokenStr = bearer[7:]
+				}
+			}
 
-		// Verify the token
-		token, err := ja.Decode(tokenStr)
-		if err != nil || !token.Valid {
-			http.Error(w, errUnauthorized.Error(), 401)
-			return
-		}
+			// Get token from cookie
+			if tokenStr == "" {
+				cookie, err := r.Cookie("jwt")
+				if err == nil {
+					tokenStr = cookie.Value
+				}
+			}
 
-		// Token is valid! save it in the context
-		c.Env["token"] = token
-		h.ServeHTTP(w, r)
+			// Token is required, cya
+			if tokenStr == "" {
+				err = errUnauthorized
+			}
+
+			// Verify the token
+			token, err := ja.Decode(tokenStr)
+			if err != nil || !token.Valid {
+				http.Error(w, errUnauthorized.Error(), 401)
+				return
+			}
+
+			// Token is valid! save it in the context
+			c.Env["token"] = token
+			h.ServeHTTP(w, r)
+		}
+		return web.HandlerFunc(fn)
 	}
-	return http.HandlerFunc(fn)
+	return f
+}
+
+func (ja *jwtAuth) Handler(h http.Handler) http.Handler {
+	return ja.Handle("")(h)
 }
 
 func (ja *jwtAuth) Encode(claims map[string]interface{}) (tokenString string, err error) {
